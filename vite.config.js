85f157099f13f06b50bbfc9b4afc1867521442c0
KARRA SAI KARTHIK <Karrasaikarthik2001@gmail.com>
2024-08-23 23:28:00 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/portfolio_website/",
+  base:"/karthik_portfolio/",
   build:{
     outDir:"docs",
   }
